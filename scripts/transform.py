47b6cc28ec944c3a06891f855f5555036be38771
Maria Regina <mreginaluzsilva@gmail.com>
2026-01-12 18:15:49 -0300

    Adiciona validacao de nulos e intervalos no transform

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,5 +1,26 @@
 import pandas as pd
 
+def calcular_status(linha):
+    """
+    Função auxiliar para aplicar as regras linha a linha.
+    """ 
+    nota = linha['nota_simulado']
+    freq = linha['frequencia']
+
+    if pd.isna(nota) or pd.isna(freq): #primeiro vê se tem dados
+        return "Dados Insuficientes"
+    
+    if (nota < 0) or (nota > 10): #vê se o dado é válido
+        return "Nota Inválida"
+    
+    if (freq < 0) or (freq > 1.0): #vê se o dado é válido
+        return "Frequência Inválida"
+    
+    if nota >= 7 and freq >= 0.75: #regra de aprovação
+        return "Aprovado"
+    else:
+        return "Reprovado"
+
 def transform_alunos(df): 
     """
     Função que define a regra de aprovação dos alunos
@@ -10,17 +31,15 @@ def transform_alunos(df):
     """
 
     #validação
-    colunas_obrigatorias = ["nota_simulado", "frequencia"]
-    for coluna in colunas_obrigatorias:
-        if coluna not in df.columns:
-            raise ValueError(f"Coluna obrigatória ausente: {coluna}")
-    
+    colunas_obrigatorias = ["nota_simulado", "frequencia", 'idade']
+    for col in colunas_obrigatorias:
+        if col not in df.columns:
+            raise ValueError(f"Coluna obrigatória ausente: {col}")
+
+    #autofill   
+    df["idade"] = df["idade"].fillna(-1)
+
     #transformação
-    df["status_aprovação"] = df.apply(
-    lambda linha: 
-        "Aprovado" 
-        if linha["nota_simulado"] >= 7 and linha["frequencia"] >= 0.75
-        else "Reprovado",
-    axis=1
+    df["status_aprovação"] = df.apply(calcular_status, axis=1
 )
     return df
