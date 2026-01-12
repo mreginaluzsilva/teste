5e34e96f3aaaee9e3d2a17f09d45f95a3b847012
Maria Regina <mreginaluzsilva@gmail.com>
2026-01-12 13:09:54 -0300

    Versão incial do pipeline ETL de alunos

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,7 +1,26 @@
 import pandas as pd
 
-def transform_alunos(df): #diferente do extract, essa função recebe dados, ela não lê arquivo nenhum
-    df["status_aprovação"] = df["nota"].apply(
-    lambda x: "Aprovado" if x >= 7 else "Reprovado"
+def transform_alunos(df): 
+    """
+    Função que define a regra de aprovação dos alunos
+    
+    Regra de aprovação:
+    - nota_simulado >= 7
+    - frequencia >= 0.75
+    """
+
+    #validação
+    colunas_obrigatorias = ["nota_simulado", "frequencia"]
+    for coluna in colunas_obrigatorias:
+        if coluna not in df.columns:
+            raise ValueError(f"Coluna obrigatória ausente: {coluna}")
+    
+    #transformação
+    df["status_aprovação"] = df.apply(
+    lambda linha: 
+        "Aprovado" 
+        if linha["nota_simulado"] >= 7 and linha["frequencia"] >= 0.75
+        else "Reprovado",
+    axis=1
 )
     return df
